753f29a2c57f398a681580f27d7cf132d5abf006
Isikov Mykyta <mykyta.isikov90@gmail.com>
2021-05-05 18:12:46 +0300

    main.js imports cleaned

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 const electron = require('electron')
-const url = require('url');
-const path = require('path')
-const Conscript = require('./models/Conscript')
 
-const {app, BrowserWindow, Menu, ipcMain} = electron
+const {app, ipcMain} = electron
 
 const mainWindowLib = require('./app-components/mainWindow')
-const searchWindowLib = require('./app-components/searchWindow')
 const resultWindowLib = require('./app-components/resultWindow')
-
-// mongoose
-const mongoose = require('mongoose');
-const { ipcRenderer } = require('electron');
 
 process.env.NODE_ENV = 'production'
 
@@ -23,16 +15,11 @@ app.on('ready', function(){
 ipcMain.on("test-signal", (event, data) => {
     console.log(`{ ${data.dateFrom}, ${data.dateTo} }`)
     event.reply("test-reply", "reply")
-    //ipcMain.removeAllListeners("test-signal")
+
     console.log("test-signal caught")
 
     resultWindow = resultWindowLib.create()
-/*
-    if (mainWindow !== null) {
-        mainWindow.destroy()
-        mainWindow = null
-    } 
-*/
+
     ipcMain.on("resultWindow-ready", (event) => {
         console.log("resultWindow is ready, dates sent: " + `{ ${data.dateFrom}, ${data.dateTo} }`)
         event.reply("dates-for-search", data)
